5cb44a0365063d9dfec730a2e9080514da74d189
egathang <elyg3672@gmail.com>
2024-03-04 13:56:25 +0300

    Factorial numbers

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,16 +1,13 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb < 0 {
+	if nb > 0 && nb < 30 {
+		factorial := 1
+		for i := 1; i <= nb; i++ {
+			factorial *= i
+		}
+		return factorial
+	} else {
 		return 0
 	}
-	factorial := 1
-
-	for i := 1; i <= nb; i++ {
-		if factorial > (1<<31-1)/i {
-			return 0
-		}
-		factorial *= i
-	}
-	return factorial
 }

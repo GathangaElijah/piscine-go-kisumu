54756bb3df75f1de3d9a79cd7ced215e2ffb8cae
egathang <elyg3672@gmail.com>
2024-03-04 15:30:52 +0300

    Recursive factorial

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,11 +1,11 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-	if nb == 1 {
-		return 1
-	}
-	if nb > 1 {
+	if nb > 0 && nb <= 20 {
 		return nb * RecursiveFactorial(nb-1)
+	} else if nb == 0 {
+		return 1
+	} else {
+		return 0
 	}
-	return 0
 }

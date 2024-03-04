8337ab5ee305369677393e2e23cde38fb2717447
egathang <elyg3672@gmail.com>
2024-03-04 13:26:13 +0300

    Factorial numbers

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -7,6 +7,9 @@ func IterativeFactorial(nb int) int {
 	factorial := 1
 
 	for i := 1; i <= nb; i++ {
+		if factorial > (1 << 31 - 1) / i {
+            return 0
+        }
 		factorial *= i
 	}
 	return factorial

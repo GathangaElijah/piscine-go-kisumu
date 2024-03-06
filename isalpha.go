dcb930bc1b8ad29b3dc1fbf13ffc2533886b8e3b
egathang <elyg3672@gmail.com>
2024-03-06 22:16:27 +0300

    Returning the alphanumerics

---
+++ isalpha.go
@@ -0,0 +1,16 @@
+package piscine
+
+func IsAlpha(s string) bool {
+	for _, value := range s {
+		// check out characters is uppercase
+		if !((value >= 'A' && value <= 'Z') ||
+			//checks out characters is lowercase
+			(value >= 'a' && value <= 'z') ||
+			// checks if the character is  a numeric
+			(value >= '0' && value <= '9'))  {
+			return false
+		}
+
+	}
+	return true
+}

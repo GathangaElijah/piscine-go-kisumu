f55a574b767b564d996f15e55342c6b5f395c971
egathang <elyg3672@gmail.com>
2024-03-07 00:02:31 +0300

    Non-printable characters

--- isprintable.go
+++ isprintable.go
@@ -2,11 +2,10 @@ package piscine
 
 func IsPrintable(s string) bool {
 	printchars := []rune(s)
-	for _,value := range printchars {
+	for _, value := range printchars {
 		if value >= 0 && value <= 31 {
 			return false
 		}
-
 	}
 	return true
 }

fffbbc324618e668ac4662f80ca439e41c69443c
egathang <elyg3672@gmail.com>
2024-03-07 00:01:44 +0300

    Non-printable characters

---
+++ isprintable.go
@@ -0,0 +1,12 @@
+package piscine
+
+func IsPrintable(s string) bool {
+	printchars := []rune(s)
+	for _,value := range printchars {
+		if value >= 0 && value <= 31 {
+			return false
+		}
+
+	}
+	return true
+}

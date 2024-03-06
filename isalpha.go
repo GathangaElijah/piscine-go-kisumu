9aeb7eca415f43fea7918b44bf07e0838e38e90b
egathang <elyg3672@gmail.com>
2024-03-06 22:22:15 +0300

    Returning the alphanumerics

--- isalpha.go
+++ isalpha.go
@@ -2,11 +2,8 @@ package piscine
 
 func IsAlpha(s string) bool {
 	for _, value := range s {
-		// check out characters is uppercase
 		if !((value >= 'A' && value <= 'Z') ||
-			//checks out characters is lowercase
 			(value >= 'a' && value <= 'z') ||
-			// checks if the character is  a numeric
 			(value >= '0' && value <= '9')) {
 			return false
 		}

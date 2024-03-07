7e6abf74803ff5032a8d6fb7f8d4049f430d1717
egathang <elyg3672@gmail.com>
2024-03-07 19:29:12 +0300

    Capitalizing characters

---
+++ capitalize.go
@@ -0,0 +1,37 @@
+package piscine
+
+func Capitalize(s string) string {
+	var output []rune
+	makeCapital := true
+	for _, value := range s {
+		if makeCapital && isLetterOrDigit(value) {
+			value = toUpperCase(value)
+			makeCapital = false
+		} else if !isLetterOrDigit(value) {
+			makeCapital = true
+		} else if !makeCapital && isLetter(value) {
+			value = toLowerCase(value)
+		}
+		output = append(output, value)
+	}
+	return string(output)
+}
+
+func isLetterOrDigit(value rune) bool {
+	return (value >= 'a' && value <= 'z') || (value >= 'A' && value <= 'Z') || (value >= '0' && value <= '9')
+}
+func isLetter(value rune) bool {
+	return (value >= 'a' && value <= 'z') || (value >= 'A' && value <= 'Z')
+}
+func toUpperCase(value rune) rune {
+	if value >= 'a' && value <= 'z' {
+		return value - 32
+	}
+	return value
+}
+func toLowerCase(value rune) rune {
+	if value >= 'A' && value <= 'Z' {
+		return value + 32
+	}
+	return value
+}

742c96515c857640754d29b12c79996a07fd1ad9
egathang <elyg3672@gmail.com>
2024-03-08 22:57:16 +0300

    Number conversion to its int

---
+++ nbrconvertalpha/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"os"
+
+	"os.*, github.com/01-edu/z01"
+)
+
+func main(){
+	args:= os.Args[1:]
+
+	upper:=false
+	if len(args) > 0 && args[0] == "--upper" {
+		upper = true
+		args = args[1:]
+	}
+
+	if len(args) == 0 {
+		return
+	}
+	for _,arg := range args{
+		n, ok := Atoi(arg)
+		if !ok || n < 1 || n > 26 {
+			z01.PrintRune(' ')
+			continue
+		}
+
+		letter := rune('a' + n-1)
+		if upper {
+			letter = rune('A' + n-1)
+		}
+		z01.PrintRune(letter)
+	}
+	z01.PrintRune('\n')
+}
+
+func Atoi(s string) (int, bool) {
+	var result int
+	var negative bool
+
+	if s[0] == '-'{
+		negative  = true
+		s = s[1:]
+	}
+
+	for _, digit := range s {
+		if digit < '0' || digit > '9'{
+			return 0, false
+		}
+		result = result*10 + int(digit-'0')
+	}
+
+	if negative {
+		result = result
+	}
+	result result, true
+}

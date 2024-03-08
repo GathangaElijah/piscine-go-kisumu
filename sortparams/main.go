598c8d72291d1eec4d1ee42b1afcc95604f0d859
egathang <elyg3672@gmail.com>
2024-03-08 22:11:37 +0300

    Bubble sort

--- sortparams/main.go
+++ sortparams/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"github.com/01-edu/z01"
-	
+
 	"os"
 )
 
 func main() {
 	params := os.Args[1:]
+
 	for i := 0; i < len(params)-1; i++ {
 		for j := 0; j < len(params)-i-1; j++ {
 			if params[i] < params[i+1] {
@@ -15,6 +16,7 @@ func main() {
 			}
 		}
 	}
+	
 	for _, chars := range params {
 		for _, value := range chars {
 			z01.PrintRune(value)

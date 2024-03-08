2293a2c11b0ecddb3bbc2523555c13edbf4a04ed
egathang <elyg3672@gmail.com>
2024-03-08 19:04:13 +0300

    More parameters

---
+++ printparams/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	argument := os.Args[1:]
+	for _, value := range argument[1:] {
+		for _, new_char{
+			z01.PrintRune(new_char)
+		}
+	}
+	z01.PrintRune('\n')
+}

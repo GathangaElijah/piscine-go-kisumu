84081e33421c9dd85a5455e805d796e7b8c89fa7
egathang <elyg3672@gmail.com>
2024-03-08 18:34:26 +0300

    The first os arguments

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -6,9 +6,9 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func main(){
+func main() {
 	arguments := os.Args[0]
-	for _,value := range arguments[2:] {
+	for _, value := range arguments[2:] {
 		z01.PrintRune(value)
 	}
 	z01.PrintRune('\n')

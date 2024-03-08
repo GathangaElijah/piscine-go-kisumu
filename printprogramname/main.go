d84444755b8b7e17b5a847df14d13a91eefb430b
egathang <elyg3672@gmail.com>
2024-03-08 18:27:11 +0300

    The first os arguments

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -2,16 +2,12 @@ package piscine
 
 import (
 	"github.com/01-edu/z01"
-
 	"os"
 )
 func PrintProgramName()  {
-
 	arguments := os.Args[0]
-	
 	for _,value := range arguments[2:]{
 		z01.PrintRune(value)
 	}
-
 	z01.PrintRune('\n')
 }

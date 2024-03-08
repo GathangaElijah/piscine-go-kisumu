6866290e01efb30f248cea2e41afc8662b8a8e3f
egathang <elyg3672@gmail.com>
2024-03-08 20:04:59 +0300

    Reverse parameters

---
+++ revparams/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main()  {
+	for i:=len(os.Args)-1; i > 0; i--{
+		new_char := os.Args[i]
+		for _,value := range new_char{
+			z01.PrintRune(value)
+		}
+	}
+	z01.PrintRune('\n')
+}

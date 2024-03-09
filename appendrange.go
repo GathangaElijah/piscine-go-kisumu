f127c63f05facf8a9859e407bfc72b6ddabd068d
egathang <elyg3672@gmail.com>
2024-03-09 11:13:42 +0300

    Appending parameters

--- appendrange.go
+++ appendrange.go
@@ -2,7 +2,7 @@ package piscine
 
 func AppendRange(min, max int) []int {
 	params := []int{}
-	nil_params := []int(nil)
+	nil_params := []int{0,0}
 	for i := min; i < max; i++ {
 		if min >= max {
 			return nil_params

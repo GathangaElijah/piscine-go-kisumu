4eeb25c71488ce8943e042b4dfaf82d779c35dbc
egathang <elyg3672@gmail.com>
2024-03-09 12:06:00 +0300

    make() funtion

--- makerange.go
+++ makerange.go
@@ -1,8 +1,9 @@
 package piscine
 
 func MakeRange(min, max int) []int {
+	var nil_params []int
 	if min >= max {
-		return nil
+		return nil_params
 	}
 	params := make([]int, max-min+1)
 	for i := min; i < max-min; i++ {

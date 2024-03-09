d16e68e1b1c4df30b7a35d4cc2985731b19a60d6
egathang <elyg3672@gmail.com>
2024-03-09 20:24:47 +0300

    concating parameters to a string

---
+++ concatparams.go
@@ -0,0 +1,12 @@
+package piscine
+
+func ConcatParams(args []string) string {
+	var params string
+	for i, value := range args {
+		if i >= 0 {
+			params += ("\n")
+		}
+		params += value
+	}
+	return params
+}

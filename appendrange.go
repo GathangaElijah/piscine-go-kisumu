d899d75e0407b78c55293d697782994f6945a938
egathang <elyg3672@gmail.com>
2024-03-09 10:50:45 +0300

    Appending parameters

---
+++ appendrange.go
@@ -0,0 +1,14 @@
+package piscine
+
+func AppendRange(min, max int) []int {
+
+	params := []int{}
+	for i := min; i < max; i++{
+		if min >= max{
+			return params
+		}else{
+			params = append(params,i)
+		}
+	}
+	return params
+}

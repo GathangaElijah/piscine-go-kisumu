79677af06377db866398a4d93c3974f1413c16de
egathang <elyg3672@gmail.com>
2024-03-09 12:02:59 +0300

    make() funtion

--- makerange.go
+++ makerange.go
@@ -4,7 +4,7 @@ func MakeRange(min, max int) []int {
 	if min >= max {
 		return nil
 	}
-	params := make([]int, max-min)
+	params := make([]int, max-min+1)
 	for i := min; i < max-min; i++ {
 		params[i] = min + i
 	}

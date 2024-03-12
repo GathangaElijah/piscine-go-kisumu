22b00ba2a4d27648a555ad6d83b4660deccaaaab
egathang <elyg3672@gmail.com>
2024-03-12 23:09:11 +0300

    Displaying the contents of a file

---
+++ displayfile/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	args := len(os.Args[1:])
+	if args == 0 {
+		fmt.Println("File name missing")
+		return
+	}
+	if args > 1 {
+		fmt.Println("Too much arguments")
+		return
+	}
+
+	//Finding the file
+
+	filename := os.Args[1]
+
+	// check is the file is there
+
+	_, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		fmt.Println("The filename is missing", filename)
+		return
+	}
+
+	// Reading the file
+
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		println("Error reading the file", err)
+		return
+	}
+
+	// Outputting the data
+
+	fmt.Println(string(content))
+}

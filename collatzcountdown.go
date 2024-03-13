a9cb7744e219cdd98ee503405f4001120a563be9
egathang <elyg3672@gmail.com>
2024-03-13 14:24:46 +0300

    Collatz conjecture

--- collatzcountdown.go
+++ collatzcountdown.go
@@ -1,17 +1,17 @@
 package piscine
 
 func CollatzCountdown(start int) int {
-if start <= 0 {
-	return -1
-}
-var noOfSteps = 0
-for start != 1{
-	if start%2 == 0{
-		start = start/2
-	} else {
-		start = (start*3)+1
+	if start <= 0 {
+		return -1
 	}
-	noOfSteps ++
-}
-return noOfSteps
+	var noOfSteps = 0
+	for start != 1 {
+		if start%2 == 0 {
+			start = start / 2
+		} else {
+			start = (start * 3) + 1
+		}
+		noOfSteps++
+	}
+	return noOfSteps
 }

4128d2e37d8bade1826e9889a033993c218fd6d0
egathang <elyg3672@gmail.com>
2024-03-14 01:26:00 +0300

    Printing from commandline

---
+++ comcheck/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	listOfArgs := os.Args[1:]
+	for _, arg := range listOfArgs {
+		if arg == "01" || arg == "galaxy" || arg == "galaxy 01" {
+			fmt.Println("Alert!!!")
+		}
+	}
+}

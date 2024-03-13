a2e31df094aee6aaad8303d8ca1e23150330ead9
egathang <elyg3672@gmail.com>
2024-03-13 10:47:59 +0300

    Hackathon qs 1

--- rot14.go
+++ rot14.go
@@ -6,7 +6,7 @@ func rot14(s string) string {
 		if char >= 'a' && char <= 'z' {
 			newString += string((char+14)%26 + 'a')
 		} else if char >= 'A' && char <= 'Z' {
-			newString += string((char+14)%26 + 'Z')
+			newString += string((char+14)%26 + 'A')
 		} else {
 			newString += string(char)
 		}

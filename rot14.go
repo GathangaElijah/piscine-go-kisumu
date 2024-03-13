25314dc6a753ed4a0a17ab46c6355f8f0abf1321
Christopher Fremond <dev@01-edu.org>
2024-03-13 07:56:49 +0000

    Update 'rot14.go'

--- rot14.go
+++ rot14.go
@@ -4,9 +4,9 @@ func Rot14(s string) string {
 	var newString string
 	for _, char := range s {
 		if char >= 'a' && char <= 'z' {
-			newString += string((char-'a')%26 + 'a')
+			newString += string(((char-'a')+14)%26 + 'a'))
 		} else if char >= 'A' && char <= 'Z' {
-			newString += string((char-'A')%26 + 'A')
+			newString += string(((char-'A')+14)%26 + 'A')
 		} else {
 			newString += string(char)
 		}

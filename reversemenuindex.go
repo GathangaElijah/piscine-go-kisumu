f472feb6530fc5d66199d045988d5d85410d5146
egathang <elyg3672@gmail.com>
2024-03-14 02:27:20 +0300

    Reverse string

--- reversemenuindex.go
+++ reversemenuindex.go
@@ -3,7 +3,7 @@ package piscine
 func ReverseMenuIndex(menu []string) []string {
 	lengthOfMenu := len(menu)
 	sortedMenu := make([]string, lengthOfMenu)
-	for i := lengthOfMenu; i >= 1; i-- {
+	for i := lengthOfMenu - 1; i >= 1; i-- {
 		sortedMenu = make([]string, i)
 	}
 	return sortedMenu

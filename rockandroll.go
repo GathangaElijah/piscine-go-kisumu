390f7d4487ee2127fb8ff8c113b127ad369ddc1a
egathang <elyg3672@gmail.com>
2024-03-14 08:47:18 +0300

    Rock and Roll

--- rockandroll.go
+++ rockandroll.go
@@ -2,17 +2,13 @@ package piscine
 
 func RockAndRoll(n int) string {
 	if n < 0 {
-		error := "error: number is negative\n"
-		return error
+		return "error: number is negative\n"
 	} else if n > 0 && n%2 == 0 && n%3 == 0 {
-		message3 := "rock and roll\n"
-		return message3
+		return "rock and roll\n"
 	} else if n > 0 && n%2 == 0 {
-		message1 := "rock\n"
-		return message1
+		return "rock\n"
 	} else if n > 0 && n%3 == 0 {
-		message2 := "roll\n"
-		return message2
+		return "roll\n"
 	} else {
 		return "error: non divisible\n"
 	}

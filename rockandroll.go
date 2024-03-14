29b317e7ed801aac251dc48e047e76877d024643
egathang <elyg3672@gmail.com>
2024-03-14 08:30:05 +0300

    Rock and Roll

--- rockandroll.go
+++ rockandroll.go
@@ -13,9 +13,6 @@ func RockAndRoll(n int) string {
 	} else if n%3 == 0 {
 		message2 := "rock\n"
 		return message2
-	} else if n%2 == 0 && n%3 == 0 {
-		message3 := "rock and roll\n"
-		return message3
 	} else {
 		return "non divisible\n "
 	}

5e376d5a52428d300838e75784f0d3e6187ed4fe
egathang <elyg3672@gmail.com>
2024-03-14 08:39:23 +0300

    Rock and Roll

--- rockandroll.go
+++ rockandroll.go
@@ -4,16 +4,16 @@ func RockAndRoll(n int) string {
 	if n < 0 {
 		error := "error: number is negative\n"
 		return error
-	} else if n%2 == 0 && n%3 == 0 {
+	} else if n > 0 && n%2 == 0 && n%3 == 0 {
 		message3 := "rock and roll\n"
 		return message3
-	} else if n%2 == 0 {
+	} else if n > 0 && n%2 == 0 {
 		message1 := "rock\n"
 		return message1
-	} else if n%3 == 0 {
-		message2 := "rock\n"
+	} else if n > 0 && n%3 == 0 {
+		message2 := "roll\n"
 		return message2
 	} else {
-		return "non divisible\n "
+		return "error: non divisible\n "
 	}
 }
